3fac42defee40d9e9d0b46748254cfd821db5234
Enjoyer Grab <93958573+animegr@users.noreply.github.com>
2021-11-25 19:13:20 +1100

    Update inject.js

--- assets/inject.js
+++ assets/inject.js
@@ -75,11 +75,19 @@ let dbAds=[
       ".content",
       ".pa15.bgwhite"
     ],
-    "position":"out-top", //out-top, out-bottom, in-top, in-bottom
-    "data" :`
-      if (location.host === "anokw.netlify.app") {
-    location.href = "https://neonime.uk";
-  };
+   "position":"out-bottom", //out-top, out-bottom, in-top, in-bottom
+    "data" :`<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- Iklan Header -->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4606499,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4606499&101" alt="frontpage hit counter" border="0"></a></noscript>
     `,
     "style":`
       width: 90%;
